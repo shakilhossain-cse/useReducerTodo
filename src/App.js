32e974ad5e57b1a484e687ae0ccb83c000cbf582
shakilhossain-cse <shakilhossainp.cse@gmail.com>
2022-02-23 17:58:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Divider,
+  FormGroup,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <FormGroup>
+        <TextField
+          id="standard-basic"
+          label="type your todo"
+          sx={{ width: "100%" }}
+          variant="outlined"
+        />
+        <Button variant="contained" sx={{ my: "1rem" }}>
+          Create
+        </Button>
+      </FormGroup>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        <ListItem alignItems="flex-start">
+          <ListItemText primary="Brunch this weekend?" />
+          <ButtonGroup size="small" aria-label="small button group">
+            <Button>Edit</Button>
+            <Button>Delete</Button>
+          </ButtonGroup>
+        </ListItem>
+
+        <Divider variant="inset" component="li" />
+      </List>
+    </Box>
   );
 }
 
